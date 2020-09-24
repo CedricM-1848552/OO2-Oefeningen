1812830bdf90c9d3d5ac6e1217e840484874da93
Cédric Meukens <cedric.meukens@icloud.com>
2020-09-24 15:25:50 +0200

    Finished opdracht_02_07

---
+++ opdracht_02_07/src/io/github/cedricmeu/Main.java
@@ -0,0 +1,10 @@
+package io.github.cedricmeu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length && i < 3; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
